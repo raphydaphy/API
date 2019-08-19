9aecdde5840bb3f7309158fcdc423d174fcfb799
Raph Hennessy <raph.hennessy@gmail.com>
2019-08-19 19:07:14 +1000

    Server tokens

--- src/main/java/de/ellpeck/rockbottom/api/net/login/IUserAccount.java
+++ src/main/java/de/ellpeck/rockbottom/api/net/login/IUserAccount.java
@@ -4,7 +4,11 @@ import java.util.UUID;
 
 public interface IUserAccount {
     String getUsername();
+
     UUID getToken();
+    UUID getServerToken();
+
     boolean isValid();
     boolean renew();
+
 }
